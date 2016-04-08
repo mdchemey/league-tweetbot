e4123d1fa75bea3634461437e2e5cdc74b45e7c9
Misha Chemey <mdcswimmer@gmail.com>
2016-04-07 20:16:28 -0400

    Update bundle-b.bat

--- bundle-b.bat
+++ bundle-b.bat
@@ -1,3 +1,4 @@
+REM deletes any existing build of checkLeague, then creates a new windowless build, copying in 'userInfo.txt' to the directory holding 'checkLeague.exe' for proper function
 rd /Q /S build,dist
 del checkLeague.spec
 pyinstaller -w checkLeague.py
